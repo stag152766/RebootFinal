5ed044c63e57fd760adfa41ab7da191ca6a0945f
stag152766 <45874547+stag152766@users.noreply.github.com>
2021-03-22 20:58:36 +0700

    add post service

---
+++ server/src/main/java/com/bulletinboard/server/controllers/PostController.java
@@ -0,0 +1,95 @@
+package com.bulletinboard.server.controllers;
+
+import com.bulletinboard.server.dto.PostDTO;
+import com.bulletinboard.server.entity.Post;
+import com.bulletinboard.server.facade.PostFacade;
+import com.bulletinboard.server.payload.response.MessageResponse;
+import com.bulletinboard.server.services.PostService;
+import com.bulletinboard.server.validations.ResponseErrorValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/post")
+@CrossOrigin
+public class PostController {
+
+    @Autowired
+    private PostFacade postFacade;
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createPost(@Valid @RequestBody PostDTO postDTO,
+                                             BindingResult bindingResult,
+                                             Principal principal) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+
+        Post post = postService.createPost(postDTO, principal);
+        PostDTO createPost = postFacade.postToPostDTO(post);
+
+        return new ResponseEntity<>(createPost, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PostDTO>> getAllPosts() {
+        List<PostDTO> postDTOList = postService.getAllPosts()
+                .stream()
+                .map(postFacade::postToPostDTO)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(postDTOList, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/user/posts")
+    public ResponseEntity<List<PostDTO>> getAllPostForUser(Principal principal) {
+        List<PostDTO> postDTOList = postService.getAllPostForUser(principal)
+                .stream()
+                .map(postFacade::postToPostDTO)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(postDTOList, HttpStatus.OK);
+    }
+
+    @PostMapping("/{postId}/{username}/favorite")
+    public ResponseEntity<PostDTO> likePost(@PathVariable("postId") String postId,
+                                            @PathVariable("username") String username) {
+        Post post = postService.favoritePost(Long.parseLong(postId), username);
+        PostDTO postDTO = postFacade.postToPostDTO(post);
+
+        return new ResponseEntity<>(postDTO, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/{postId}/delete")
+    public ResponseEntity<MessageResponse> deletePost(@PathVariable("postId") String postId, Principal principal) {
+        postService.deletePost(Long.parseLong(postId), principal);
+        return new ResponseEntity<>(new MessageResponse("Post was deleted"), HttpStatus.OK);
+    }
+
+    @GetMapping("user/favorites")
+    public ResponseEntity<List<PostDTO>> getFavoritePostForUser(Principal principal){
+        List<PostDTO> postDTOList = postService.getFavoritePostForUser(principal)
+                .stream()
+                .map(postFacade::postToPostDTO)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(postDTOList, HttpStatus.OK);
+    }
+
+
+}
