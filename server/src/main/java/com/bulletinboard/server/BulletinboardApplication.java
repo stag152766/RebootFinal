c9efb884aca9697fc30a743071a47d1563abd6d9
stag152766 <45874547+stag152766@users.noreply.github.com>
2021-03-22 13:59:35 +0700

    init project

---
+++ server/src/main/java/com/bulletinboard/server/BulletinboardApplication.java
@@ -0,0 +1,13 @@
+package com.bulletinboard.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinboardApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/bulletinboard/server/BulletinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulletinboard.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
